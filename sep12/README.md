91ddfbb814fec56a82d9922665f214152e5a7463
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-09-12 11:31:51 -0700

    first half

--- sep12/README.md
+++ sep12/README.md
@@ -1,1 +1,1 @@
-- Functions
+# Functions/Methods creation
--- sep12/ex.cpp
+++ sep12/ex.cpp
@@ -1,12 +1,68 @@
 #include <iostream>
 
-// Funtion
-int pow2(int x)
+const double PI = 3.14; // Global variable example (always constants)
+
+// Funtion/method creation
+int pow2(int); // Prototype
+
+int myPow(int, int = 2); // default parameter has to be after the non-default parameter
+// int myPow(int) // You can also overload
+
+// Function with string parameter
+// The & makes the function reference the variable instead of creating a new one (if it doesn't have 'const' it has to be a variable, not literal).
+// The 'const' makes it not changeable (safer when referencing).
+void print(const std::string &str)
 {
+    std::cout << str << std::endl;
 }
 
+void max_min(int a, int b, int &max, int &min)
+{
+    max = a;
+    min = b;
+    if (a < b)
+    {
+        max = b;
+        min = a;
+    }
+}
+
+// Function documentation:
+// What it does
+// Inputs (Assumptions about the input too (ex: a >= 0))
+// Output
 int main()
 {
+    std::cout << pow2(3) << std::endl;
+
+    std::cout << myPow(2, 2) << std::endl;
+    std::cout << myPow(2, 4) << std::endl;
+    std::cout << myPow(2, 6) << std::endl;
+    std::cout << myPow(2, 8) << std::endl;
+
+    std::string s = "Hello World";
+    print(s);
+
+    // Manipulating variables with functions
+    int max, min;
+    max_min(2, 7, max, min);
+    std::cout << max << " " << min << std::endl;
 
     return 0;
+}
+
+// Actual funtion
+int pow2(int x)
+{
+    return x * x;
+}
+
+int myPow(int x, int n)
+{
+    int result = x;
+    for (int i = 1; i < n; i++)
+    {
+        result *= x;
+    }
+    return result;
 }
