df5f6f00418910c80c54fa2796bdf3d2a36359dd
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-09-28 20:42:32 -0700

    worked on assignment 2
    there was an attempt :(

---
+++ assignments/asgn02/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void swapValues(char arr[], int a, int b)
+{
+    char temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+void printComb(char arr[])
+{
+}
+
+void genComb(char arr[], int i = 0)
+{
+    if (i < 4)
+    {
+    }
+}
+
+int main()
+{
+    char arr[] = {'A', 'B', 'C', 'D'};
+    genComb(arr);
+}
