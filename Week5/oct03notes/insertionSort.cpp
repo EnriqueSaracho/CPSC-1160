9e30b3891e16fe243a060623aa3dbe39e4d01546
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-10-03 12:16:00 -0700

    oct02notes

---
+++ Week5/oct03notes/insertionSort.cpp
@@ -0,0 +1,48 @@
+// Insertion Sort (recursive).
+#include <iostream>
+#include <vector>
+
+void insertionSort(std::vector<int> &list)
+{
+    int count = 0;
+    for (unsigned int i = 1; i < list.size(); i++)
+    {
+        int tmp = list[i];
+        int j;
+        for (j = i - 1; j >= 0 && ++count && tmp < list[j]; j--)
+        {
+            list[j + 1] = list[j];
+        }
+        list[j + 1] = tmp;
+    }
+    std::cout << count << std::endl;
+    return;
+}
+
+// Recursively (you don't want to do this).
+void recInsertionSort(std::vector<int> &list, int i = 1)
+{
+    if (i == list.size())
+    {
+        return;
+    }
+    int tmp = list[i];
+    int j;
+    for (j = i - 1; j >= 0 && tmp < list[j]; j--)
+    {
+        list[j + 1] = list[j];
+    }
+    list[j + 1] = tmp;
+    recInsertionSort(list, i + 1);
+}
+
+int main()
+{
+    std::vector<int> myList = {4, 7, 1, 3, 13, -6, 9, 12};
+    // insertionSort(myList);
+    recInsertionSort(myList);
+    for (int i = 0; i < myList.size(); i++)
+    {
+        std::cout << myList[i] << " ";
+    }
+}
---
+++ Week5/oct03notes/lab03reveiw.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+// Lab03 review:
+int path(std::vector<std::vector<int>> &m, int r = 0, int c = 0, int p_s = 0)
+{
+    if (r == m.size() || c == m[r].size() || m[r][c] == 0)
+    {
+        return -1;
+    }
+    p_s += m[r][c];
+    if (r == m.size() - 1 && c == m[r].size() - 1)
+    {
+        return p_s;
+    }
+
+    int a = path(m, r + 1, c, p_s);
+    int b = path(m, r, c + 1, p_s);
+
+    if (a == -1 && b == -1)
+    {
+        return -1;
+    }
+    if (a == -1)
+    {
+        return b;
+    }
+    if (b == -1)
+    {
+        return a;
+    }
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
+
+int main()
+{
+    std::vector<std::vector<int>> m = {
+        {1, 2, 4, 0, 4},
+        {4, 3, 3, 2, 4},
+        {0, 0, 2, 2, 1},
+        {1, 0, 2, 2, 1},
+        {1, 4, 2, 3, 2}};
+    std::cout << path(m) << std::endl;
+}
