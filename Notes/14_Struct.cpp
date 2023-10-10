52d7ee90780faf35d426a042d6725a76c1e76d63
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-10-10 12:10:25 -0700

    oct10 full class notes

--- Notes/14_Struct.cpp
+++ Notes/14_Struct.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <ctime>
 
 struct point
 {
-    int x = 0; // 0 is the default value.
-    int y = 0;
+    int x = rand() % 14; // 0 is the default value.
+    int y = rand() % 15;
 
     void print()
     {
         std::cout << x << " " << y << std::endl;
+    }
+
+    // the same functions but inside the struct
+    bool comp(point &q)
+    {
+        return x == q.x & y == q.y;
+    }
+    point mid(point &q)
+    {
+        return point{(x + q.x), (y + q.y)};
+    }
+
+    // returns the distance between two points
+    double dist(point &q)
+    {
+        return std::sqrt(std::pow((x + q.x), 2) + std::pow((y + q.y), 2));
     }
 };
 
@@ -18,8 +36,35 @@ bool comp(point &p, point &q)
     return p.x == q.x & p.y == q.y;
 }
 
+// returning a point from a function
+point mid(point &p, point &q)
+{
+    return point{(p.x + q.x), (p.y + q.y)};
+}
+
+// Find the to points in a vector closest to each other and returns distance
+double minDist(std::vector<point> &v)
+{
+    if (v.size() < 2)
+        return 0;
+
+    double min = v[0].dist(v[1]);
+    double dist;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = i + 1; j < v.size(); j++)
+        {
+            dist = v[i].dist(v[j]);
+            if (dist < min)
+                min = dist;
+        }
+    }
+    return min;
+}
+
 int main()
 {
+    srand(time(0));
     point p = {9, 2};
     point q = p; // makes a deep copy
     // point &q = p; // makes a reference
@@ -30,4 +75,25 @@ int main()
     p.y = 5;
     std::cout << p.x << " " << p.y << std::endl;
     std::cout << q.x << " " << q.y << std::endl;
+
+    point w = mid(p, q);
+    w.print();
+
+    // Calling the functions but from inside the struct
+    point m = p.mid(q);
+    m.print();
+    std::cout << p.comp(m) << std::endl;
+
+    // Calling distance function
+    std::cout << w.dist(m) << std::endl;
+
+    // With Vectors
+    std::cout << std::endl;
+    std::cout << "Vector part" << std::endl;
+    std::vector<point> v_p(10);
+    for (auto &x : v_p)
+    {
+        x.print();
+    }
+    std::cout << "Minimum distance: " << minDist(v_p) << std::endl;
 }
