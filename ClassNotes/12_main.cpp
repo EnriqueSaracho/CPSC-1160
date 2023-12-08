02c047cbd92878220416b56f30bb47b816ca6228
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2023-12-07 22:11:26 -0800

    PracticeExams/PracticeFinal/ updated
    practiced merge sort

--- ClassNotes/12_main.cpp
+++ ClassNotes/12_main.cpp
@@ -27,28 +27,6 @@ void insertionSort(std::vector<int> &list)
     return;
 }
 
-void insSort(std::vector<int> &list)
-{
-    for (int i = 1; i < list.size(); i++)
-    {
-        for (int k = 0; k < list.size(); k++)
-        {
-            std::cout << list[k] << " ";
-        }
-        std::cout << std::endl;
-        for (int j = 0; j < i; j++)
-        {
-            if (list[j] > list[i])
-            {
-                int tmp = list[j];
-                list[j] = list[i];
-                list[i] = tmp;
-                break;
-            }
-        }
-    }
-}
-
 // Recursively (you don't want to do this).
 void recInsertionSort(std::vector<int> &list, int i = 1)
 {
@@ -70,8 +48,7 @@ int main()
 {
     std::vector<int> myList = {4, 7, 1, 3, 13, -6, 9, 12};
     // insertionSort(myList);
-    // recInsertionSort(myList);
-    insSort(myList);
+    recInsertionSort(myList);
     for (int i = 0; i < myList.size(); i++)
     {
         std::cout << myList[i] << " ";
